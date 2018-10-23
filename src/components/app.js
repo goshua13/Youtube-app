5493b9a08e7aae28f80f28dc1fa42b726a247516
Joshua Aragon <joshuaaragon@Joshuas-MacBook-Pro.local>
2018-10-22 21:27:24 -0600

    commit dis

---
+++ src/components/app.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search';
+
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+
+const API_KEY = "AIzaSyCxobR3HUbmY7kJOCy0AEkixM1MERhSbTY";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [],
+      selectedVideo: null 
+    };
+    this.videoSearch('wavemusic');
+  }
+
+  videoSearch(term) {
+    YTSearch ({key: API_KEY, term: term }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className='app'> 
+        <SearchBar onSearchTermChange={term => {this.videoSearch(term)}}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+export default App;
